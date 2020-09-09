31057f6458b19565b22726cc5f2c2121f73ce580
Tae-gyu Yun <tgyyun@gmail.com>
2020-09-09 18:42:19 +0930

    Update server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ app.use("/api/post", require("./routes/api/post"));
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
   // Set static folder
-  app.use(express.static("cleint/build"));
+  app.use(express.static("client/build"));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__direname, "client", "build", "index.html"));
   });
